9eb979a122e6a286b6aefd2e87f653a89a127c77
Shyaan <Shayanbahrainy@gmail.com>
2024-12-02 09:32:22 -0800

    updated config

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ bind = '0.0.0.0:443'  # this is where you declare on which address your
                          # gunicorn app is running.
                          # Basically where Nginx will forward the request to
 
-pidfile = '/var/run/gunicorn/mysite.pid' # create a simple pid file for gunicorn. 
+pidfile = '/var/run/gunicorn/ravyn.pid' # create a simple pid file for gunicorn. 
 
 user = 'user'          # the user gunicorn will run on
 
 daemon = False          # this is only to tell gunicorn to deamonize the server process
 
-errorlog = '/var/log/gunicorn/error-mysite.log'    # error log
+errorlog = '/var/log/gunicorn/error-ravyn.log'    # error log
 
-accesslog = '/var/log/gunicorn/access-mysite.log'  # access log
+accesslog = '/var/log/gunicorn/access-ravyn.log'  # access log
 
-proc_name = 'gunicorn-mysite'            # the gunicorn process name
+proc_name = 'gunicorn-ravyn'            # the gunicorn process name
