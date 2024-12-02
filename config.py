cfa7f66934ed33e3ed80528da5791dbde1791557
Shyaan <Shayanbahrainy@gmail.com>
2024-12-02 09:40:26 -0800

    cut

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 "Config for Gunicorn"
-workers = 3              # number of workers Gunicorn will spawn 
-
 bind = '0.0.0.0:443'  # this is where you declare on which address your 
                          # gunicorn app is running.
                          # Basically where Nginx will forward the request to
