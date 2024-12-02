603ffecd3d5334d3ea6d1059ba832d8b9ffcb985
Shyaan <Shayanbahrainy@gmail.com>
2024-12-02 09:36:41 -0800

    Shortened config

--- config.py
+++ config.py
@@ -4,15 +4,3 @@ workers = 3              # number of workers Gunicorn will spawn
 bind = '0.0.0.0:443'  # this is where you declare on which address your 
                          # gunicorn app is running.
                          # Basically where Nginx will forward the request to
-
-pidfile = '/var/run/gunicorn/ravyn.pid' # create a simple pid file for gunicorn. 
-
-user = 'opc'          # the user gunicorn will run on
-
-daemon = False          # this is only to tell gunicorn to deamonize the server process
-
-errorlog = 'gunicorn/error-ravyn.log'    # error log
-
-accesslog = 'gunicorn/access-ravyn.log'  # access log
-
-proc_name = 'gunicorn-ravyn'            # the gunicorn process name
