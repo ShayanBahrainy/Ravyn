61a956c793aeab1f174e31e58535de4d811d89c2
Shyaan <Shayanbahrainy@gmail.com>
2024-12-02 09:33:22 -0800

    set user to opc

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ bind = '0.0.0.0:443'  # this is where you declare on which address your
 
 pidfile = '/var/run/gunicorn/ravyn.pid' # create a simple pid file for gunicorn. 
 
-user = 'user'          # the user gunicorn will run on
+user = 'opc'          # the user gunicorn will run on
 
 daemon = False          # this is only to tell gunicorn to deamonize the server process
 
