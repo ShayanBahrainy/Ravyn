379e92d23d037c95b6f861bad001ad3bac905b3e
Shyaan <Shayanbahrainy@gmail.com>
2024-12-05 20:59:25 -0800

    Deleted corrupting files

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ with open("Client.secret") as f:
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
-BETA_ACCOUNTS = "BETA_ACCOUNTS.txt"
+BETA_ACCOUNTS = None
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
 
